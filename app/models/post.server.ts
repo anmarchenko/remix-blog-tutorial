7383b37e3a1495ad03c28720bca151ef21defd59
Andrey Marchenko <andrei.marchenko@protonmail.com>
2023-03-05 16:30:23 +0100

    render post

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -5,3 +5,7 @@ export type { Post } from "@prisma/client";
 export async function getPosts() {
   return prisma.post.findMany();
 }
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
