e4fbcea9b7c41233665afb69144baf10fbe43cdc
Andrey Marchenko <andrei.marchenko@protonmail.com>
2023-03-08 07:22:25 +0100

    js disabling submit button

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,8 +1,8 @@
 import type { ActionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { json, redirect } from "@remix-run/node";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
 import invariant from "tiny-invariant";
+
 import { createPost } from "~/models/post.server";
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
@@ -35,6 +35,8 @@ export const action = async ({ request }: ActionArgs) => {
 
 export default function NewPost() {
   const errors = useActionData<typeof action>();
+  const navigation = useNavigation();
+  const isCreating = Boolean(navigation.state == "submitting");
 
   return (
     <Form method="post">
@@ -75,6 +77,7 @@ export default function NewPost() {
         <button
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
         >
           Create Post
         </button>
